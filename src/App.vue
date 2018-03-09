672810321c4ae64a92760814add44afbe390b5d9
Kamar MEDDAH <Kamar.meddah@gmail.com>
2018-03-09 12:42:26 +0100

    Initial  commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template >
+<div>
+<h1>it worked</h1>
+</div>
+</template>
+
+<script lang ='ts'>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+  },
+})
+export default class App extends Vue {}
+</script>
+
